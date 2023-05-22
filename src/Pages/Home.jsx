2f4f42b06489d5745ed1079f5427fd2b6f61dbf9
Domingos Canhanga <canhanga96@gmail.com>
2023-05-22 13:06:14 +0100

    :rocket: separating styles

--- src/Pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-
-export function Home() {
-  return <>
-  <h1>Lista de presença</h1>
-  <input type="text" name="" id="" placeholder="Digite o nome"/>
-  <button type="submit">Adicionar</button>
-  </>;
-}
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import style from './style.module.scss';
+export function Home() {
+  return (
+    <>
+      <h1>Lista de presença</h1>
+      <input type="text" name="" id="" placeholder="Digite o nome" />
+      <button type="submit">Adicionar</button>
+    </>
+  );
+}
